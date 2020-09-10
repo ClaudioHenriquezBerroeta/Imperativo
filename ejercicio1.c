db83d2200e13a0483d068f6c8d25cf22f88929d0
Claudio Henríquez <63313873+ClaudioHenriquezBerroeta@users.noreply.github.com>
2020-09-10 12:11:07 -0300

    scanf char*

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
-// %i variables enteras
-// %f variables reales
-// %c variables caracter
-// %s variables de cadenas de caracteres
+#include <stdlib.h>
+// %i variables enteras (utilizar & en scanf)
+// %f variables reales (utilizar & en scanf)
+// %c variables caracter (utilizar & en scanf)
+// %s variables de cadenas de caracteres (no utilizar & en scanf y definir como char *)
 
 int main(){
 	int var_int;
 	float var_float;
 	char var_char;
-	printf("Ingrese un número entero: \n");
-	scanf("%c",&var_char);
-	printf("El número entero ingresado es: %c \n",var_char);
+	char *var_string = malloc(sizeof(char)*10);
+
+
+
+	printf("Ingrese una palabra: \n");
+	scanf("%s",var_string);
+	printf("La palabra ingresada es: %s \n",var_string);
 	return 0;
 }
