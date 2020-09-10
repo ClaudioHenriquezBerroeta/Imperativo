a11d75054687a3e302bbda62fab51efc0e4ecd2e
Claudio Henríquez <63313873+ClaudioHenriquezBerroeta@users.noreply.github.com>
2020-09-10 11:59:11 -0300

    scanf

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+// %i variables enteras
+// %f variables reales
+// %c variables caracter
+// %s variables de cadenas de caracteres
+
 int main(){
-	printf("Hola mundo");
+	int var_int;
+	float var_float;
+	char var_char;
+	printf("Ingrese un número entero: \n");
+	scanf("%c",&var_char);
+	printf("El número entero ingresado es: %c \n",var_char);
 	return 0;
 }
